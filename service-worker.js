c45f1bcc4fdae4fb6f71fe0171ac4e01ea1f2d3a
ryymike <a735626287@qq.com>
2021-05-09 12:30:35 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,17 @@
 importScripts("https://cdn.myun.info/workbox-v4.3.1/workbox-sw.js", "precache-manifest.7a02ff48f6096f394253bc79532daa5c.js");
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js');
-// workbox.precaching([
-//     // 注册成功后要立即缓存的资源列表
-// ])
+workbox.precaching([
+    // 注册成功后要立即缓存的资源列表
+])
 
-// html的缓存策略
-// workbox.routing.registerRoute(
-//     new RegExp('.*\.html'),
-// workbox.strategies.networkFirst()
-// );
-//
-// workbox.routing.registerRoute(
-//     new RegExp('.*\.(?:js|css)'),
-//     workbox.strategies.cacheFirst()
-// );
+workbox.routing.registerRoute(
+    new RegExp('.*\.html'),
+    workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
+    new RegExp('.*\.(?:js|css)'),
+    workbox.strategies.networkFirst()
+);
 
