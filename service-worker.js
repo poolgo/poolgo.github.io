9980db05e5ae1817e7570082e2a020602078be5f
ryymike <a735626287@qq.com>
2021-05-10 15:36:30 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,34 @@
-importScripts("https://cdn.myun.info/workbox-v4.3.1/workbox-sw.js", "precache-manifest.d73b6a2f31e3392e9ed898210ac2c053.js");
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js');
-workbox.precaching([
-    // 注册成功后要立即缓存的资源列表
-])
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.html'),
-    workbox.strategies.networkFirst()
+importScripts(
+  "precache-manifest.3844713a4cd54304779593e210631aea.js"
 );
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.(?:js|css)'),
-    workbox.strategies.networkFirst()
-);
+workbox.core.setCacheNameDetails({prefix: "PoolGo"});
 
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
