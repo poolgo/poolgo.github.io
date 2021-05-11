4cccd77532afcd3d817a210561f27dd5d1a4281a
ryymike <a735626287@qq.com>
2021-05-11 18:04:10 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.472e2023f3c47ef574d5e6808146c8a8.js"
+  "precache-manifest.c1930c1d8016b5af97d40a365f071e13.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "PoolGo"});
